78996ff7d482a09b849b0247110bccaf27851731
Your Name <you@example.com>
2026-04-21 00:18:03 +0300

    user routes for the controllers

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const {registerUser} = require('../controllers/userCtr');
+const {registerUser, loginUser, loginStatus, getUser, logoutUser, loginAsSeller, getUserBalance, getAllUser, estimateIncome} = require('../controllers/userCtr');
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logged-in", loginStatus);
+router.get("/logout", logoutUser);
+router.post("seller", loginAsSeller);
+router.get("/getuser", protect, getUser);
+router.get("sell-amount", protect, getUserBalance);
 
-
+router.get("/estimate-income", protect, isAdmin, estimateIncome);
+router.get("/users", protect, isAdmin, getAllUser);
 module.exports = router;
