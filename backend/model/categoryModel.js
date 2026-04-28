fa0966e57020864c1740a92ba0a00b3f111363dd
Your Name <you@example.com>
2026-04-28 12:34:12 +0300

    changed exprots casing

--- backend/model/categoryModel.js
+++ backend/model/categoryModel.js
@@ -15,5 +15,5 @@ const categorySchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Category = mongoose.model("Category", categorySchema);
-module.exports = Category;
+const category = mongoose.model("Category", categorySchema);
+module.exports = category;
